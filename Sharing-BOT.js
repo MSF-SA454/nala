e4ad143de2754a375d97aabc4578d050dd8c5ea4
MSF-SA454 <41073730+MSF-SA454@users.noreply.github.com>
2019-02-08 19:18:12 +0300

    Add files via upload

---
+++ Sharing-BOT.js
@@ -0,0 +1,20 @@
+﻿const Discord = require('discord.js');
+const A7MD = new Discord.Client();
+
+console.log("BOT ONLINE");
+ 
+A7MD.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(` 
+**
+سلام عليكم 
+سيرفر لكسيرويوس باستضافتكم
+رتب سيلر بارخص الاسعار
+نحتاج دعمكم,
+
+                               [ https://discord.gg/X7cKTk ] 
+الدعووة خاصة لك ... [ ${member}  ]
+**`) 
+}).catch(console.error)
+})
+A7MD.login('token');
